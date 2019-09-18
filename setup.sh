bc84354c3bc3ebe41e2798d26646668b2bccebfd
Shao jie He <49934147+ShaojieHe@users.noreply.github.com>
2019-09-18 22:43:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 yum install wget
 wget -N --no-check-certificate https://raw.githubusercontent.com/ShaojieHe/handyScript/master/ssr.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/ShaojieHe/handyScript/master/tcp.sh
+wget -N --no-check-certificate https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar axvf LATEST.tar.gz
 yum update
