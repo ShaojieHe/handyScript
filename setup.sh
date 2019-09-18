5a506d5780f2e6536c06707738a16de5c5497ce9
Shao jie He <49934147+ShaojieHe@users.noreply.github.com>
2019-09-19 00:27:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,12 @@ wget -N --no-check-certificate https://raw.githubusercontent.com/ShaojieHe/handy
 wget -N --no-check-certificate https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
 tar axvf LATEST.tar.gz
 yum update -y
+yum install openssh-server iptables-services
+iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
+iptables -A OUTPUT -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+iptables -A INPUT -p tcp --dport 20 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1919 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2048 -j ACCEPT
+iptables -A INPUT -p tcp -j DROP
