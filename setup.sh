d2488228f9c9aa04d3cc1699edff19ceed225bbe
Shao jie He <49934147+ShaojieHe@users.noreply.github.com>
2019-09-19 00:23:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-yum install wget
+yum install wget -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/ShaojieHe/handyScript/master/ssr.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/ShaojieHe/handyScript/master/tcp.sh
 wget -N --no-check-certificate https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
 tar axvf LATEST.tar.gz
-yum update
+yum update -y
