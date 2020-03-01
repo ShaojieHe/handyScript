15e92e133f23fdb543e11408e4998b66ae969f18
ShaojieHe <741185493@qq.com>
2020-03-01 18:49:33 +0800

    updated and working fine

--- fileRenlink.sh
+++ fileRenlink.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
+#Change "realinkedfile" to any thing you like
 Srcdir=""
 Desdir=""/relinkedfile
 echo $Desdir
-#trap 'rm -f "${Desdir}"/"${file_name}"' EXIT
+trap "rm -f "${Desdir}"/new_*" EXIT
 type inotifywait > /dev/null || exit 1
 
 if [[ ${1} != "-y" ]]; then exit 1 ;fi
 
 if [[ ! -d "${Desdir}" ]]; then mkdir "$Desdir"; fi
 
-    inotifywait -rmq --format '%Xe %w%f' -e modify,create,delete,attrib,move --exclude "FileList.txt" ${Srcdir} | while read file
-    do
+inotifywait -mq --format '%Xe %w%f' -e modify,create,delete,attrib,move --exclude "fileRenlink.sh" ${Srcdir} | while read file
+do
     file_operations=$(echo "$file" | awk '{print $1}')
     file_name=$(echo "$file" | awk '{print $2}')
     file_name=${file_name##*\/}
     if [[ ${file_operations} == "CREATE" ]]; then
     {
+        echo $old
+        rm -f "${Desdir}"/"${old}"
         echo Linked new file
-        ln -f "${Srcdir}"/"${file_name}" "${Desdir}"/"${file_name}"
+        ln -f "${Srcdir}"/"${file_name}" "${Desdir}"/"new_${file_name}" > /dev/null 2>&1
+        unset old
+        old=new_${file_name}
     }
     fi
     if [[ ${file_operations} == "DELETE" ]]; then
     {
         echo Trying delete linked file
-        rm -f "${Desdir}"/"${file_name}"
+        rm -f "${Desdir}"/"new_${file_name}"
     }
     fi
-    done
+done
