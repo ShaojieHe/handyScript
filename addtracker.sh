7eb1f788258a9688c30e7c0a9d73248fa461c962
ShaojieHe <741185493@qq.com>
2020-03-14 18:17:54 +0800

    update

--- addtracker.sh
+++ addtracker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-dir="/home/ftpuser/extra_disk/.aria2/"
+dir="/home/ftpuser/extra_disk/.aria2"
 cd $dir
+if [ ! -f ${dir}/aria2.session  ]; then touch ${dir}/aria2.session ; fi
 sed '/bt-tracker/d' aria2.conf >> aria2.conf.new
 echo -n "bt-tracker=" >> aria2.conf.new
 curl "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt" | sed ":a;N;s/\n/,/g;ta" | sed 's/,,/,/g' | sed 's/','*$//g' >> aria2.conf.new
