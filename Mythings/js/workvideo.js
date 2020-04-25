dc76e059077fdd8c35c9ad8feeaddbfa62c677e5
ShaojieHe <741185493@qq.com>
2020-04-25 16:28:49 +0800

    update for video dash

--- Mythings/js/workvideo.js
+++ Mythings/js/workvideo.js
@@ -1,13 +1,26 @@
 'use strict'
 let dp
-let addr = document.getElementById('addr')
-
+let type
+let addr = {"default_addr":[{}],"value":''}
+addr.default_addr["normal"]="https://ftp.qwq.es/share/test.mp4"
+addr.default_addr["dash"]="https://ftp.qwq.es/share/test_dash.mp4/output.mpd"
+type="normal"
+document.getElementById('normal').onclick = () =>{
+    type="normal"
+    document.getElementById('addr').setAttribute("placeholder",addr.default_addr.normal)
+    }
+document.getElementById('dash').onclick = () =>{
+    type="dash"
+    document.getElementById('addr').setAttribute("placeholder",addr.default_addr.dash)
+    }
 function toplay(){
-    if (addr.value === '') {
-      addr.value="https://ftp.qwq.es/share/test.mp4"
+    addr["value"] = document.getElementById('addr').value;
+    if (addr["value"] === '') {
+      type === "normal" && (addr.value=addr.default_addr.normal);
+      type === "dash" && (addr.value=addr.default_addr.dash);
     }
     console.log(addr.value);
-    
+if (type === 'normal'){    
     dp = new DPlayer({
     container: document.getElementById('dplayer'),
     lang: 'zh-cn',
@@ -17,5 +30,32 @@ function toplay(){
         url: addr.value,
     },
 });
-     
+}
+if (type === 'dash'){
+        dp = new DPlayer({
+    container: document.getElementById('dplayer'),
+    lang: 'zh-cn',
+    theme: '#FADFA3',
+    volume: 0.3,
+    live: false,
+    video: {        
+        url: addr.value,
+        type: 'shakaDash',
+        customType: {
+            shakaDash: function(video, player) {
+                var src = video.src;
+                var playerShaka = new shaka.Player(video);
+                playerShaka.configure({
+                    streaming:{
+                        bufferingGoal: 30,
+                        rebufferingGoal: 15,
+                        bufferBehind: 15
+                    }
+                })
+                playerShaka.load(src);
+            },
+        },
+    },
+});
+    }
 }
